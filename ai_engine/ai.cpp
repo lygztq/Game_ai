e96d03e9cf7985509c3cede6ce9afd7bf70f4e8c
lygztq <lygztq@sjtu.edu.cn>
2017-10-26 13:15:40 +0800

    Day3, a little bit clever, change the candidate set

--- ai_engine/ai.cpp
+++ ai_engine/ai.cpp
@@ -2,65 +2,84 @@
 * @Author: lygztq
 * @Date:   2017-10-20 11:30:12
 * @Last Modified by:   lygztq
-* @Last Modified time: 2017-10-21 21:03:13
+* @Last Modified time: 2017-10-26 13:14:25
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <cstring>
 #include <limits.h>
+#include <algorithm>
+#include <vector>
 // for test begin
 #include <time.h>
 #include <random>
 #include <windows.h>
 // for test end
 #include "ai.h"
+
+/*some inline function*/
 inline int random(int x){return rand()%x + 1;}
 inline int different_color(int color){return 3 - color;}
+inline int huffman_distance(point p1, point p2) {return abs(p1.x-p2.x)+abs(p1.y-p2.y);}
+inline bool is_neighbor(point p1, point p2) {return huffman_distance(p1, p2) <= NEIGHBOR_DISTANCE;}
 inline int abs(int x)
 {
 	if(x<0) return ~x+1;
 	return x; 
 }
-inline bool is_nab(int x1, int y1, int x2, int y2)
-{
-	return abs(x1-x2)+abs(y1-y2)<=2;
-}
+
+/* cmp for weight point */
+bool cmp_weight_point(weight_point p1, weight_point p2){return p1.weight>p2.weight;}
 
 /* board class */
+
+int **board::copy_board(int **b)
+{
+	int **ans = new int *[BOARD_SIZE+1];
+	for(int i=1;i<=BOARD_SIZE;++i)
+	{
+		ans[i] = new int [BOARD_SIZE+1];
+		for(int j=1;j<=BOARD_SIZE;++j)
+			ans[i][j] = b[i][j];
+	}
+	return ans;
+}
+
 board::board()
 {
 	board_look = new int * [BOARD_SIZE+1];
+	consider_weight = new int *[BOARD_SIZE+1];
 	for(auto i=1;i<=BOARD_SIZE;++i)
 	{
+		consider_weight[i] = new int [BOARD_SIZE+1];
 		board_look[i] = new int [BOARD_SIZE+1];
 		for(auto j=1;j<=BOARD_SIZE;++j)
+		{
+			consider_weight[i][j] = 0;
 			board_look[i][j] = 0;
+		}
 	}
-	search_grid_domain.x_start = BOARD_SIZE/2;
-	search_grid_domain.y_start = BOARD_SIZE/2;
-	search_grid_domain.x_end = BOARD_SIZE/2+1;
-	search_grid_domain.y_end = BOARD_SIZE/2+1;
-
+	consider_weight[BOARD_SIZE/2][BOARD_SIZE/2]=1; 
 	empty_number = BOARD_SIZE * BOARD_SIZE;
 }
 
-board::board(int **b)
+/*board::board(int **b)
 {
 	empty_number = BOARD_SIZE * BOARD_SIZE;
 	board_look = new int * [BOARD_SIZE+1];
-	search_grid_domain.x_start = BOARD_SIZE/2;
-	search_grid_domain.y_start = BOARD_SIZE/2;
-	search_grid_domain.x_end = BOARD_SIZE/2+1;
-	search_grid_domain.y_end = BOARD_SIZE/2+1;
-	
 	if(b==NULL)
+	{
 		for(auto i=1;i<=BOARD_SIZE;++i)
 		{
 			board_look[i] = new int [BOARD_SIZE+1];
 			for(auto j=1;j<=BOARD_SIZE;++j)
 				board_look[i][j] = 0;
 		}
+		point initial_point(BOARD_SIZE/2,BOARD_SIZE/2);
+		candidate_points.push_back(initial_point);
+	}
 	else
+	{
 		for(auto i=1;i<=BOARD_SIZE;++i)
 		{
 			board_look[i] = new int [BOARD_SIZE+1];
@@ -81,18 +100,23 @@ board::board(int **b)
 				}
 			}
 		}
-}
+	}
+}*/
 
 board::board(const board &b)
 {
 	board_look = new int *[BOARD_SIZE+1];
+	consider_weight = new int *[BOARD_SIZE+1];
 	for(auto i=1;i<=BOARD_SIZE;++i)
 	{
+		consider_weight[i] = new int [BOARD_SIZE+1];
 		board_look[i] = new int [BOARD_SIZE+1];
 		for(auto j=1;j<=BOARD_SIZE;++j)
+		{
+			consider_weight[i][j] = b.consider_weight[i][j];
 			board_look[i][j] = b.board_look[i][j];
+		}
 	}
-	search_grid_domain = b.search_grid_domain;
 	empty_number = b.empty_number;
 }
 
@@ -103,36 +127,71 @@ board & board::operator=(const board &b)
 
 	if(board_look==NULL)
 	{
-		search_grid_domain.x_start = BOARD_SIZE/2;
-		search_grid_domain.y_start = BOARD_SIZE/2;
-		search_grid_domain.x_end = BOARD_SIZE/2+1;
-		search_grid_domain.y_end = BOARD_SIZE/2+1;
-
+		consider_weight = new int *[BOARD_SIZE+1];
 		board_look = new int *[BOARD_SIZE+1];
 		for(auto i=1;i<=BOARD_SIZE;++i)
 		{
+			consider_weight[i] = new int [BOARD_SIZE+1];
 			board_look[i] = new int [BOARD_SIZE+1];
 			for(auto j=1;j<=BOARD_SIZE;++j)
+			{
+				consider_weight[i][j] = b.consider_weight[i][j];
 				board_look[i][j] = b.board_look[i][j];
+			}
 		}
-		empty_number = BOARD_SIZE * BOARD_SIZE;
 	}
 	else
-	{
 		for(auto i=1;i<=BOARD_SIZE;++i)
 			for(auto j=1;j<=BOARD_SIZE;++j)
+			{
+				consider_weight[i][j] = b.consider_weight[i][j];
 				board_look[i][j] = b.board_look[i][j];
-		empty_number = b.empty_number;
-		search_grid_domain = b.search_grid_domain;
-	}
+			}
+	
+	empty_number = b.empty_number;
 	return *this;
 }
 
 board::~board()
 {
 	for(auto i=1;i<=BOARD_SIZE;++i)
+	{
+		delete [] consider_weight[i];
 		delete [] board_look[i];
+	}
+	delete [] consider_weight;
 	delete [] board_look;
+}
+
+void board::add_neighbor(int x, int y)
+{
+	int nx,ny;
+	int dx[12] = {-2,-1,-1,-1,0,0,0,0,1,1,1,2};
+	int dy[12] = {0,-1,0,1,-2,-1,1,2,-1,0,1,0};
+	for(int i=0;i<12;++i)
+	{
+		nx = x + dx[i];
+		ny = y + dy[i];
+		if(!in_board(nx, ny)) continue;
+		++consider_weight[nx][ny];
+		//if(!is_empty(nx, ny)) continue;
+		//if(find(candidate_points.begin(), candidate_points.end(), np)!=candidate_points.end()) continue;
+		//candidate_points.push_back(np);
+	}
+}
+
+void board::remove_neighbor(int x, int y)
+{
+	int nx,ny;
+	int dx[12] = {-2,-1,-1,-1,0,0,0,0,1,1,1,2};
+	int dy[12] = {0,-1,0,1,-2,-1,1,2,-1,0,1,0};
+	for(int i=0;i<12;++i)
+	{
+		nx = x + dx[i];
+		ny = y + dy[i];
+		if(!in_board(nx,ny)) continue;
+		--consider_weight[nx][ny];
+	}
 }
 
 bool board::add_a_stone(int color, int x, int y)
@@ -143,16 +202,19 @@ bool board::add_a_stone(int color, int x, int y)
 		return false;
 	}
 	board_look[x][y] = color;
-	--empty_number; 
+	add_neighbor(x,y);
+	--empty_number;
 
-	if(x<=search_grid_domain.x_start)
-		search_grid_domain.x_start = (x>3)?(x-2):1;
-	else if(x>=search_grid_domain.x_end)
-		search_grid_domain.x_end = (x+2<BOARD_SIZE)?(x+2):BOARD_SIZE;
-	if(y<=search_grid_domain.y_start)
-		search_grid_domain.y_start = (y>3)?(y-2):1;
-	else if(y>=search_grid_domain.y_end)
-		search_grid_domain.y_end = (y+2<BOARD_SIZE)?(y+2):BOARD_SIZE;
+	return true;
+}
+
+bool board::remove_a_stone(int x, int y)
+{
+	if(!in_board(x,y)) return false;
+	board_look[x][y] = EMPTY;
+	remove_neighbor(x,y);
+	++empty_number;
+
 	return true;
 }
 
@@ -201,13 +263,21 @@ bool board::has_five_line(int color)
 	return false;
 }
 
-void board::test_show()
+void board::test_show(int mode)
 {
 	printf("test show\n");
+	if(!mode)
 	for(auto x=1;x<=BOARD_SIZE;++x)
 	{
 		for(auto y=1;y<=BOARD_SIZE;++y)
 			printf("%d ", board_look[x][y]);
+		printf("\n");
+	}
+	else
+	for(auto x=1;x<=BOARD_SIZE;++x)
+	{
+		for(auto y=1;y<=BOARD_SIZE;++y)
+			printf("%d ", consider_weight[x][y]);
 		printf("\n");
 	}
 }
@@ -225,7 +295,8 @@ int ai::line_evaluation(int **board_look,
 						int end_x, 
 						int end_y, 
 						int x_step, 
-						int y_step)
+						int y_step,
+						int judge_color)
 {
 	int ptr_x = start_x, ptr_y = start_y; // current position
 	int tmp_x, tmp_y;
@@ -236,8 +307,8 @@ int ai::line_evaluation(int **board_look,
 	{
 		length = 0;
 		empty_end = 0;
-		// get the first stone with ai's color
-		while(board_look[ptr_x][ptr_y]!=color && (ptr_x!=end_x || ptr_y!=end_y))
+		// get the first stone with judge color
+		while(board_look[ptr_x][ptr_y]!=judge_color && (ptr_x!=end_x || ptr_y!=end_y))
 		{
 			ptr_x += x_step; 
 			ptr_y += y_step;
@@ -252,13 +323,13 @@ int ai::line_evaluation(int **board_look,
 		bool over_boundary = false; 
 		tmp_x = ptr_x; 
 		tmp_y = ptr_y;
-		while(board_look[tmp_x][tmp_y]==color)
+		while(board_look[tmp_x][tmp_y]==judge_color)
 		{
 			++length;
 			tmp_x += x_step; tmp_y += y_step;
 			if(tmp_x==end_x && tmp_y==end_y)
 			{
-				if(board_look[tmp_x][tmp_y]==color) ++length;
+				if(board_look[tmp_x][tmp_y]==judge_color) ++length;
 				else if (board_look[tmp_x][tmp_y]==EMPTY) ++empty_end;
 				break;
 			}
@@ -273,7 +344,7 @@ int ai::line_evaluation(int **board_look,
 		}
 		else {ptr_x = tmp_x; ptr_y = tmp_y;}
 
-		if(length>=5) evaluate_value_sum += GET_FIVE;
+		if(length>=5)  return INT_MAX;//evaluate_value_sum += GET_FIVE;
 		if(empty_end==1) evaluate_value_sum += DEATH[length];
 		else if(empty_end==2) evaluate_value_sum += LIVE[length];
 		if(over_boundary) break;
@@ -284,23 +355,34 @@ int ai::line_evaluation(int **board_look,
 int ai::board_evaluation(int **board_look)
 {
 	int evaluation_sum = 0;
+	int player_color = different_color(color);
 	// x-dim
 	for(int x=1;x<=BOARD_SIZE;++x)
-		evaluation_sum += line_evaluation(board_look, x, 1, x, BOARD_SIZE, NO_MOVE, INCREASE);
+	{
+		evaluation_sum += line_evaluation(board_look, x, 1, x, BOARD_SIZE, NO_MOVE, INCREASE, color);
+		evaluation_sum -= line_evaluation(board_look, x, 1, x, BOARD_SIZE, NO_MOVE, INCREASE, player_color);
+	}
 	// y-dim
 	for(int y=1;y<=BOARD_SIZE;++y)
-		evaluation_sum += line_evaluation(board_look, 1, y, BOARD_SIZE, y, INCREASE, NO_MOVE);
+	{
+		evaluation_sum += line_evaluation(board_look, 1, y, BOARD_SIZE, y, INCREASE, NO_MOVE, color);
+		evaluation_sum -= line_evaluation(board_look, 1, y, BOARD_SIZE, y, INCREASE, NO_MOVE, player_color);
+	}
 	// diagonal
 	for(int x=1;x<=BOARD_SIZE;++x)
 	{
-		evaluation_sum += line_evaluation(board_look, x, 1, 1, x, DECREASE, INCREASE);
-		evaluation_sum += line_evaluation(board_look, x, 1, BOARD_SIZE, BOARD_SIZE+1-x, INCREASE, INCREASE);
+		evaluation_sum += line_evaluation(board_look, x, 1, 1, x, DECREASE, INCREASE, color);
+		evaluation_sum += line_evaluation(board_look, x, 1, BOARD_SIZE, BOARD_SIZE+1-x, INCREASE, INCREASE, color);
+		evaluation_sum -= line_evaluation(board_look, x, 1, 1, x, DECREASE, INCREASE, player_color);
+		evaluation_sum -= line_evaluation(board_look, x, 1, BOARD_SIZE, BOARD_SIZE+1-x, INCREASE, INCREASE, player_color);
 	}
 
 	for(int y=2;y<=BOARD_SIZE;++y)
 	{
-		evaluation_sum += line_evaluation(board_look, BOARD_SIZE, y, y, BOARD_SIZE, DECREASE, INCREASE);
-		evaluation_sum += line_evaluation(board_look, 1, y, BOARD_SIZE+1-y, BOARD_SIZE, INCREASE, INCREASE);
+		evaluation_sum += line_evaluation(board_look, BOARD_SIZE, y, y, BOARD_SIZE, DECREASE, INCREASE, color);
+		evaluation_sum += line_evaluation(board_look, 1, y, BOARD_SIZE+1-y, BOARD_SIZE, INCREASE, INCREASE, color);
+		evaluation_sum -= line_evaluation(board_look, BOARD_SIZE, y, y, BOARD_SIZE, DECREASE, INCREASE, player_color);
+		evaluation_sum -= line_evaluation(board_look, 1, y, BOARD_SIZE+1-y, BOARD_SIZE, INCREASE, INCREASE, player_color);
 	}
 	return evaluation_sum;
 }
@@ -310,25 +392,45 @@ int ai::max_value(board &b, int &alpha, int &beta, int &next_x, int &next_y, int
 	if(depth==search_depth)
 		return board_evaluation(b.get_board());
 
-	bbox search_domain = b.search_grid_domain;
+	// get the condidates set
+	std::vector<weight_point> candidates;
+	for(int x=1;x<=BOARD_SIZE;++x)
+		for(int y=1;y<=BOARD_SIZE;++y)
+		{
+			if(b.consider_weight[x][y]!=0 && b.is_empty(x,y))
+			{
+				weight_point np(x,y,b.consider_weight[x][y]);
+				candidates.push_back(np);
+			}
+		}
+	sort(candidates.begin(),candidates.end(),cmp_weight_point);
 
 	int value = INT_MIN;
-	for(int x=search_domain.x_start;x<=search_domain.x_end;++x)
-		for(int y=search_domain.y_start;y<=search_domain.y_end;++y)
+	int num_of_candidates = candidates.size();
+	for(int i=0;i<num_of_candidates;++i)
+	{
+		int x = candidates[i].x, y = candidates[i].y;
+		b.add_a_stone(color,x,y);
+		int nnx,nny;
+		int next_value = min_value(b, alpha, beta, nnx, nny, depth+1);
+		b.remove_a_stone(x,y);
+		if(value<next_value)
 		{
-			if(!b.is_empty(x,y)) continue;
-			board next_board(b);
-			next_board.add_a_stone(color,x,y);
-			int nnx,nny;
-			int next_value = min_value(next_board, alpha, beta, nnx, nny, depth+1);
-			if(value<next_value)
+			value = next_value;
+			next_x = x; next_y = y;
+		}
+		else if(value==next_value)
+		{
+			int propobility = random(10);
+			if(propobility<=2)
 			{
 				value = next_value;
 				next_x = x; next_y = y;
 			}
-			if(beta<=value) return value;
-			if(alpha<value) alpha = value;
 		}
+		if(beta<=value) return value;
+		if(alpha<value) alpha = value;
+	}
 	return value;
 }
 
@@ -337,24 +439,46 @@ int ai::min_value(board &b, int &alpha, int &beta, int &next_x, int &next_y, int
 	if(depth==search_depth)
 		return board_evaluation(b.get_board());
 
-	bbox search_domain = b.search_grid_domain;
-	int value = INT_MAX;
-	for(int x=search_domain.x_start;x<=search_domain.x_end;++x)
-		for(int y=search_domain.y_start;y<=search_domain.y_end;++y)
+	// get candidates
+	std::vector<weight_point> candidates;
+	for(int x=1;x<=BOARD_SIZE;++x)
+		for(int y=1;y<=BOARD_SIZE;++y)
 		{
-			if(!b.is_empty(x,y)) continue;
-			board next_board(b);
-			next_board.add_a_stone(color,x,y);
-			int nnx,nny;
-			int next_value = max_value(next_board, alpha, beta, nnx, nny, depth+1);
-			if(value>next_value)
+			if(b.consider_weight[x][y]!=0 && b.is_empty(x,y))
+			{
+				weight_point np(x,y,b.consider_weight[x][y]);
+				candidates.push_back(np);
+			}
+		}
+	sort(candidates.begin(),candidates.end(),cmp_weight_point);
+
+	int value = INT_MAX;
+	int num_of_candidates = candidates.size();
+	for(int i=0;i<num_of_candidates;++i)
+	{
+		int x = candidates[i].x, y = candidates[i].y;
+		b.add_a_stone(different_color(color),x,y);
+		int nnx,nny;
+		int next_value = max_value(b, alpha, beta, nnx, nny, depth+1);
+		b.remove_a_stone(x,y);
+		if(value>next_value)
+		{
+			value = next_value;
+			next_x = x; next_y = y;
+		}
+		else if (value==next_value)
+		{
+			int propobility = random(10);
+			if(propobility<=2)
 			{
 				value = next_value;
 				next_x = x; next_y = y;
 			}
-			if(alpha>=value) return value;
-			if(beta>value) beta = value;
 		}
+		if(alpha >= value) return value;
+		if(beta>value) beta = value;
+
+	}
 	return value;
 }
 
@@ -385,6 +509,7 @@ int main(void)
 	int state = current_board.has_winner();
 	while(state==0 || state==3)
 	{
+		//current_board.test_show(1);
 		board_ai.next_step(current_board);
 		state = current_board.has_winner();
 		if(state==1 || state==2) break;
@@ -399,3 +524,12 @@ int main(void)
 	system("PAUSE");
 	return EXIT_SUCCESS;
 }
+
+
+/**
+1. change candidate set ok
+2. debug
+3. alpha-beta cut should sort first ok
+4. choose state with same evaluation value randomly ok
+5. Why are you(ai) so stupid?
+*/
--- ai_engine/ai.h
+++ ai_engine/ai.h
@@ -17,11 +17,12 @@ const int BLACK = 1;
 const int WHITE = 2;
 const int BOARD_FULL = 3;
 const int BOARD_SIZE = 15;
+const int NEIGHBOR_DISTANCE = 2;
 
 /* some constant for evaluation OR shape names and values*/
 const int LIVE[5] = {0,10,100,1000,10000};
 const int DEATH[5] = {0,1,10,100,1000};
-const int GET_FIVE = 100000;
+const int GET_FIVE = 10000000;
 const int NO_VALUE = 0;
 
 /* Constant for direction */
@@ -37,29 +38,63 @@ struct bbox
 	int y_end;
 };
 
+struct point
+{
+	int x;
+	int y;
+	int id;
+	point(){x=0;y=0;id=0;}
+	point(int init_x, int init_y):x(init_x),y(init_y){id = (init_x-1)*BOARD_SIZE+init_y;}
+	point(int init_id):id(init_id)
+	{
+		x = init_id/BOARD_SIZE+1;
+		y = (init_id-1)%BOARD_SIZE+1;
+	}
+};
+
+struct weight_point
+{
+	int x;
+	int y;
+	int id;
+	int weight;
+	weight_point(){x=0;y=0;id=0;weight=0;}
+	weight_point(int init_x, int init_y, int init_weight):x(init_x),y(init_y),weight(init_weight){id = (init_x-1)*BOARD_SIZE+init_y;}
+	weight_point(int init_id, int init_weight):id(init_id),weight(init_weight)
+	{
+		x = init_id/BOARD_SIZE+1;
+		y = (init_id-1)%BOARD_SIZE+1;
+	}
+};
+
 /* the board class */
 class board
 {
 private:
 	int ** board_look;
+	  // 0 for not consider
 	int empty_number;
 
-
 	bool has_five_line(int color);
+	void add_neighbor(int x, int y);
+	void remove_neighbor(int x, int y);
+	int **copy_board(int **b);
 public:
-	bbox search_grid_domain;
+	int ** consider_weight;
+	//vector<point> candidate_points;
 
 	board();
 	~board();
 	board(const board &b);
-	board(int ** b);
+	// board(int ** b);
 	board & operator=(const board &b);
 	int ** get_board(){return board_look;}
 	int has_winner(); /*0: no winner;	1: BLACK;	2: WHITE;	3: full board */
 	bool add_a_stone(int color, int x, int y);
+	bool remove_a_stone(int x, int y);
 	bool in_board(int x,int y);
 	bool is_empty(int x, int y){return board_look[x][y]==EMPTY;}
-	void test_show();
+	void test_show(int mode=0);
 };
 
 /* the ai class */
@@ -69,7 +104,7 @@ private:
 	int color;
 	int search_depth;
 
-	int line_evaluation(int **board_look, int start_x, int start_y, int end_x, int end_y, int x_step, int y_step);
+	int line_evaluation(int **board_look, int start_x, int start_y, int end_x, int end_y, int x_step, int y_step, int judge_color);
 	int max_value(board &b, int &alpha, int &beta, int &next_x, int &next_y, int depth);
 	int min_value(board &b, int &alpha, int &beta, int &next_x, int &next_y, int depth);
 public:
